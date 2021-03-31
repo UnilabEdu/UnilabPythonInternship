057625d04c48044d0c0156c7a61d88b213ea1b27
RegiusPythonidae <kvara9990@yahoo.com>
2021-03-31 16:23:50 +0400

    Added products and templates for displaying the products

--- Chapter5_Flask_Templates/Projects/Giorgi_Kvaratskhelia/app.py
+++ Chapter5_Flask_Templates/Projects/Giorgi_Kvaratskhelia/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from resources import item_dictionary
 
 app = Flask(__name__)
 
@@ -6,6 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def display_home_page():
     return render_template("index.html")
+
+
+@app.route('/catalog/<string:product_type>')
+def display_catalog_page(product_type):
+    data = item_dictionary[product_type]
+    return render_template("catalog.html", catalog_data=data)
 
 
 if __name__ == '__main__':
