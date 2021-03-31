a39824344439f5b92d5751f9fd8b17029a61e93b
RegiusPythonidae <kvara9990@yahoo.com>
2021-03-31 18:25:59 +0400

    Added HTML inheritance

--- Chapter5_Flask_Templates/Projects/Giorgi_Kvaratskhelia/app.py
+++ Chapter5_Flask_Templates/Projects/Giorgi_Kvaratskhelia/app.py
@@ -12,8 +12,15 @@ def display_home_page():
 @app.route('/catalog/<string:product_type>')
 def display_catalog_page(product_type):
     data = item_dictionary[product_type]
-    return render_template("catalog.html", catalog_data=data)
+    return render_template("catalog.html", catalog_data=data, product_type=product_type)
 
+
+@app.route('/catalog/<string:product_type>/<string:product_name>')
+def display_item_page(product_type, product_name):
+    for product in item_dictionary[product_type]:
+        if product_name in product['model']:
+            item_data = product
+            return render_template("item.html", product=item_data)
 
 if __name__ == '__main__':
     app.run()
