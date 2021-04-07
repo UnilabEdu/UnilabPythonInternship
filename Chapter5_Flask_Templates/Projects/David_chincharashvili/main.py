3b81f17ae53b5941837f55c05bd46c835828a012
David <Davidchincharashvili@gmail.com>
2021-04-07 15:01:19 +0400

    marged with unilab

--- Chapter5_Flask_Templates/Projects/David_chincharashvili/main.py
+++ Chapter5_Flask_Templates/Projects/David_chincharashvili/main.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, length, Email
 import requests
 import json
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "this_is_secret_key"
+
+
+class FormName(FlaskForm):
+    name = StringField("Name")
+    email = StringField("Email", [DataRequired(), length(max=100, min=5), Email("Field should be real Email")])
+    textarea = TextAreaField("Message", [DataRequired(), length( min=5)])
+    submit = SubmitField("Send")
+
 
 @app.route('/')
 def home():
@@ -17,9 +29,14 @@ def about():
     return render_template('about.html', data=data)
 
 
-@app.route('/contact_us')
+@app.route('/contact_us', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    form = FormName(request.form)
+    if form.validate_on_submit():
+        email = form.email.data
+        name = form.name.data
+        textarea = form.textarea.data
+    return render_template('contact.html', form=form)
 
 
 if __name__ == '__main__':
