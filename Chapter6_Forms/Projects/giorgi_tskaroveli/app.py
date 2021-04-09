8c4de38afa5d2f6560039460ee885a81578be5a9
tskaro <tskaro@gmail.com>
2021-04-09 22:12:23 +0400

    added 2 step registration forms

--- Chapter6_Forms/Projects/giorgi_tskaroveli/app.py
+++ Chapter6_Forms/Projects/giorgi_tskaroveli/app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, session
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, RadioField
 from wtforms.validators import DataRequired, length, Email
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "mySECRETkey"
+
 
 class registration_form(FlaskForm):
     email = StringField("შეიყვანე ელექტრონული ფოსტა", [DataRequired(), length(min=4), Email()])
@@ -14,8 +15,20 @@ class registration_form(FlaskForm):
     submit = SubmitField("Next")
 
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
+class registration_form2(FlaskForm):
+    address = StringField("შეიყვანე მისამართი", [DataRequired()])
+    department = StringField("შეიყვანე დეპარტამენტი", [DataRequired()])
+    shift = RadioField("სასურველი მორიგეობის განრიგი",
+                       choices=[
+                           (16, "16 საათიანი მორიგეობა"),
+                           (24, "24 საათიანი მორიგეობა"),
+                           (8, "დღის სამსახური")
+                       ])
+    submit = SubmitField("Next")
+
+
+@app.route('/step1', methods=['GET', 'POST'])
+def step1():
     email = None
     first_name = None
     last_name = None
@@ -23,15 +36,31 @@ def home():
     form = registration_form()
 
     if form.validate_on_submit():
-        email = form.email.data
-        first_name = form.first_name.data
-        last_name = form.last_name.data
-        form.email.data = ''
-        form.first_name.data = ''
-        form.last_name.data = ''
+        session['email'] = form.email.data
+        session['first_name'] = form.first_name.data
+        session['last_name'] = form.last_name.data
+        return redirect(url_for('step2'))
 
     return render_template("home.html", form=form, email=email, first_name=first_name, last_name=last_name)
 
 
+@app.route('/step2', methods=['GET', 'POST'])
+def step2():
+    address = None
+    department = None
+    shift = None
+
+    form = registration_form2()
+
+    if form.validate_on_submit():
+        session['address'] = form.address.data
+        session['department'] = form.department.data
+        session['shift'] = form.shift.data
+
+        return session['shift']
+
+    return render_template("step2.html", form=form, address=address, department=department, shift=shift)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port="4000")
