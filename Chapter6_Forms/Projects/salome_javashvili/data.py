62525b3920ef8b6c81c014e8fba95350bed7c321
salomej899 <salome.javashvili.3@iliauni.edu.ge>
2021-04-15 21:14:15 +0400

    WTForms

---
+++ Chapter6_Forms/Projects/salome_javashvili/data.py
@@ -0,0 +1,9 @@
+pathogens = ['Choose', 'Bacteria', 'Virus', 'Fungi', 'Parasite']
+pathogens_ge = ['აირჩიეთ', 'ბაქტერია', 'ვირუსი', 'სოკო', 'პარაზიტი']
+
+
+regions = ['Georgia', 'Tbilisi', 'Kakheti', 'Inner Kartli', 'Lower Kartli', 'Mtskheta-Mtianeti', 'Imereti',
+           'Samegrelo-Upper Svaneti', 'Ratcha-Lechkhumi/ Lower Svaneti', 'Samtskhe-Javakheti', 'Guria', 'Adjara']
+
+regions_ge = ['საქართველო', 'თბილისი', 'კახეთი', 'შიდა ქართლი', 'ქვემო ქართლი', 'მცხეთა-მთიანეთი', 'იმერეთი',
+              'სამეგრელო-ზემო სვანეთი', 'რაჭა-ლეჩხუმი/ ქვემო სვანეთი', 'სამცხე-ჯავახეთი', 'გურია', 'აჭარა']
