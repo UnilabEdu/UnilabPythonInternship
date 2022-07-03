ee332086cbd0ece959177348df8653dbe19b514e
Giga Amiridze <flameyungoxx@gmail.com>
2022-07-04 00:39:13 +0400

    Added products items and its generator using macro

--- Chapter5_Flask_Templates/Projects/giga_amiridze/app.py
+++ Chapter5_Flask_Templates/Projects/giga_amiridze/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+# Resources
 from resources.pages import pages
+from resources.arrival_products import arrival_products
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html", pages=pages)
+    return render_template("index.html", pages=pages, items=arrival_products)
 
 @app.route('/shop')
 def shop():
---
+++ Chapter5_Flask_Templates/Projects/giga_amiridze/resources/arrival_products.py
@@ -0,0 +1,30 @@
+arrival_products = [
+    {
+        "product_img": "https://assets.website-files.com/5f58cd55dfdef5400ad6ef28/5f5e12e5dfcb5d158f886014_item-36.jpg",
+        "product_name": "comfort eyewear",
+        "category": "unisex eyewear",
+        "price": "$ 34.00 usd",
+        "old_price": "$ 59.00 usd"
+    },
+    {
+        "product_img": "https://assets.website-files.com/5f58cd55dfdef5400ad6ef28/5f5d59a633781621b763c998_item-25.jpg",
+        "product_name": "weezbee rose gold",
+        "category": "lady's bag",
+        "price": "$ 19.99 usd",
+        "old_price": "$ 59.99 usd"
+    },
+    {
+        "product_img": "https://assets.website-files.com/5f58cd55dfdef5400ad6ef28/5f5ce62f0ee399e2b6510b0c_trinh-minh-th-dQ_06MscOeM-unsplash.jpg",
+        "product_name": "vivio brown",
+        "category": "women's shoe",
+        "price": "$ 44.99 usd",
+        "old_price": "$ 79.99 usd"
+    },
+    {
+        "product_img": "https://assets.website-files.com/5f58cd55dfdef5400ad6ef28/5f5d5ac40ee3994a9e520a73_item-17.jpg",
+        "product_name": "melvin kicks",
+        "category": "men's shoe",
+        "price": "$ 299.99 usd",
+        "old_price": "$ 359.99 usd"
+    }
+]
