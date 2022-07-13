59b850c66d5d0a5eb1889d8163e5fbe132f58bfa
Giga Amiridze <flameyungoxx@gmail.com>
2022-07-13 16:58:19 +0400

    [Feature] Added optimization of navigation pages using macro

--- Chapter8_DatabaseInViews/Projects/giga_amiridze/app.py
+++ Chapter8_DatabaseInViews/Projects/giga_amiridze/app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
+from resources.pages import nav_bar_pages
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', pages=nav_bar_pages)
+
+@app.route('/add')
+def add():
+    return render_template('add.html', pages=nav_bar_pages)
+
+@app.route('/items_list')
+def items_list():
+    return render_template('list.html', pages=nav_bar_pages)
+
+@app.route('/update')
+def update():
+    return render_template('update.html', pages=nav_bar_pages)
+
+@app.route('/delete')
+def delete():
+    return render_template('delete.html', pages=nav_bar_pages)
 
 if __name__ == '__main__':
     app.run(port=7777, debug=True)
---
+++ Chapter8_DatabaseInViews/Projects/giga_amiridze/resources/pages.py
@@ -0,0 +1,22 @@
+nav_bar_pages = [
+    {
+        "endpoint": "home",
+        "name": "Home"
+    },
+    {
+        "endpoint": "add",
+        "name": "Create"
+    },
+    {
+        "endpoint": "items_list",
+        "name": "Read"
+    },
+    {
+        "endpoint": "update",
+        "name": "Update"
+    },
+    {
+        "endpoint": "delete",
+        "name": "Delete"
+    }
+]
