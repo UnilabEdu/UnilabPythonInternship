fb6add63348b6724ed9b76f591c9b43af6151f89
daniel <danieli.gatenadze.1@iliauni.edu.ge>
2022-07-13 19:41:36 +0400

    added files

---
+++ Chapter6_Forms/Projects/daniel_gatenadze/forms.py
@@ -0,0 +1,3 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, EmailField, PasswordField, SubmitField, RadioField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
