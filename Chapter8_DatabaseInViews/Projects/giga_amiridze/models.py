7ce32207f5c9616b860c7355b9b9ffbf9a45cb59
Giga Amiridze <flameyungoxx@gmail.com>
2022-07-15 19:49:28 +0400

    Added DB view methods

--- Chapter8_DatabaseInViews/Projects/giga_amiridze/models.py
+++ Chapter8_DatabaseInViews/Projects/giga_amiridze/models.py
@@ -7,11 +7,5 @@ class Student(db.Model):
     age = db.Column(db.Integer, nullable=False)
     email = db.Column(db.String(30), nullable=False)
 
-    def __init__(self, first_name, last_name, age, email):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-        self.email = email
-
     def __repr__(self):
         return f"Student: {self.first_name}"
