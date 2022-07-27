991c1b5b34886792e77d48ede50b2e81b92b2fc0
Giga Amiridze <flameyungoxx@gmail.com>
2022-07-27 19:41:17 +0400

    Added extensions and configs files

--- Chapter9_Structuring/Projects/giga_amiridze/src/__init__.py
+++ Chapter9_Structuring/Projects/giga_amiridze/src/__init__.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask
+from src.config import Config
+from src.extensions import db, migrate
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'my_secret_key'
+    app.config.from_object(Config)
+    register_extensions(app)
     register_blueprints(app)
 
     return app
+
+def register_extensions(app):
+    db.init_app(app)
+    migrate.init_app(app, db)
 
 def register_blueprints(app):
     from src.users.views import users_blueprint
