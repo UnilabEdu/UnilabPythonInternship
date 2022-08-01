d25811475f0a3972f6517af6cf877ce984ba6f6b
Giga Amiridze <flameyungoxx@gmail.com>
2022-08-01 13:01:42 +0400

    Added header and footer with its styles

--- Chapter10_User/Projects/giga_amiridze/src/__init__.py
+++ Chapter10_User/Projects/giga_amiridze/src/__init__.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'my_secret_key'
     register_blueprints(app)
+
+    @app.route('/')
+    def home():
+        return render_template('base.html')
 
     return app
 
