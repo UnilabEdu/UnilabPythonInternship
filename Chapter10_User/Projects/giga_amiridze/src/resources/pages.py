c1c009bbd15d798d3e8157b6f092068a313c277a
Giga Amiridze <flameyungoxx@gmail.com>
2022-08-01 19:21:42 +0400

    Added title generator using macro

--- Chapter10_User/Projects/giga_amiridze/src/resources/pages.py
+++ Chapter10_User/Projects/giga_amiridze/src/resources/pages.py
@@ -2,25 +2,31 @@ pages = [
     {
         "endpoint": "home",
         "name": "Home",
+        "title": "Home",
     },
     {
         "endpoint": "post.add",
         "name": "Create",
+        "title": "Create Post",
     },
     {
         "endpoint": "post.list",
         "name": "Read",
+        "title": "Posts List",
     },
     {
         "endpoint": "post.update",
         "name": "Update",
+        "title": "Update Post",
     },
     {
         "endpoint": "post.delete",
         "name": "Delete",
+        "title": "Delete Post",
     },
     {
         "endpoint": "profile.profile",
         "name": "Profile",
+        "title": "Profile",
     },
 ]
