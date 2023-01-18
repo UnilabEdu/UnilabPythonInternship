50fd6bd1275d2030510f17c5b1e456073e2a7dc2
Ruso kvesitadze <kvesitadzeruso@gmail.com>
2023-01-18 10:04:21 +0400

    add structuring

--- Chapter9_Structuring/Projects/Ruso_Kvesitadze/flask-factory/melomane/__init__.py
+++ Chapter9_Structuring/Projects/Ruso_Kvesitadze/flask-factory/melomane/__init__.py
@@ -2,16 +2,21 @@ from flask import Flask
 from melomane.config import Config
 from melomane.views.main.routes import main_blueprint
 from melomane.views.registration.routes import registration_blueprint
+from melomane.commands import init_db
 from melomane.extensions import db
 
 
-BLUEPRINTS = [main_blueprint, registration_blueprint]
+
+BLUEPRINTS = [ main_blueprint, registration_blueprint]
+
+COMMANDS = [init_db ]
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     register_blueprints(app)
     register_extensions(app)
+    register_commands(app)
     return app
 
 def register_blueprints(app):
@@ -20,3 +25,8 @@ def register_blueprints(app):
 
 def register_extensions(app):
     db.init_app(app)
+
+
+def register_commands(app):
+    for command in COMMANDS:
+        app.cli.add_command(command)
