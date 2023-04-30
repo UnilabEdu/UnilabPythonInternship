bf2130c52e67e55189d1733a9a4819c5ea7fb2e9
RegiusPythonidae <79371694+RegiusPythonidae@users.noreply.github.com>
2023-05-01 03:16:42 +0400

    Reorganized Folders

--- Chapter9_Structuring/projects/maria_veshapidze/app/pages_list.py
+++
@@ -1,8 +0,0 @@
-nav_bar_pages_list = (
-    ("home", "Home",),
-    ("about", "About us"),
-    ("contact", "Contact Info"),
-    ("common.registration", "Registration"),
-    ("common.delete_user", "Delete"),
-    ("students.students_list", "Students' list")
-)
