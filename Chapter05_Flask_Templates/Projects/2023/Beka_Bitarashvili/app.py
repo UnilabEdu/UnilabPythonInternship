088ca7beb3d6a71e3eac3d8dc9ddf149c225cdac
Beka Bitarashvili <bekabitarashvili@gmail.com>
2023-05-09 19:41:46 +0400

    Flask Templates, Basic Web Page

---
+++ Chapter05_Flask_Templates/Projects/2023/Beka_Bitarashvili/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run()
