9f3205412685f45bd438844af907e7629072b079
RegiusPythonidae <79371694+RegiusPythonidae@users.noreply.github.com>
2023-05-22 21:07:41 +0400

    After Lecture

--- Chapter10_User/SandBox/flask-admin/app/extensions.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_admin import Admin
-
-
-db = SQLAlchemy()
-migrate = Migrate()
-admin = Admin()
--- Chapter10_User/SandBox/wekzeug_demo.py
+++
@@ -1,9 +0,0 @@
-from werkzeug.security import check_password_hash, generate_password_hash
-
-hashed_pass = generate_password_hash('bestkeptsecret')
-
-if __name__ == "__main__":
-    print(hashed_pass)
-
-    check = check_password_hash(hashed_pass, 'bestkeptsecret')
-    print(f'Result: {check}')
