562d26c351a05d22bfda99bcc13be25c1cffc6c7
Levan Kolbaia <leo.kolbaia@gmail.com>
2023-06-19 18:05:16 +0400

    factory_pattern restructure

--- Chapter09_Structuring/Projects/2023/Levan_Kolbaia/src/__init__.py
+++ Chapter09_Structuring/Projects/2023/Levan_Kolbaia/src/__init__.py
@@ -1,33 +1,32 @@
 from flask import Flask
-from src.extension import db, migrate
+from src.extensions import db, migrate
 from src.config import Config
+from src.views import main_blueprint, carbset_blueprint, cards_blueprint
 from src.commands import init_db
 
-BLUEPRINTS = [main_blueprint,cards_blueprint,carbs_blueprint]
+BLUEPRINTS = [main_blueprint, carbset_blueprint, cards_blueprint]
 COMMANDS = [init_db]
 
 def create_app():
-
     app = Flask(__name__)
-    app.config_from_object(Config)
-
-    register_extension(app)
+    app.config.from_object(Config)
+    register_extensions(app)
     register_blueprints(app)
     register_commands(app)
 
     return app
 
-def register_extension(app):
+
+def register_extensions(app):
     db.init_app(app)
 
-    migrate.init_app(app,db)
+    migrate.init_app(app, db)
+
 
 def register_blueprints(app):
     for blueprint in BLUEPRINTS:
         app.register_blueprint(blueprint)
 
-
 def register_commands(app):
     for command in COMMANDS:
-        app.cli.register_command(command)
-
+        app.cli.add_command(command)
--- Chapter09_Structuring/Projects/2023/Levan_Kolbaia/src/models/user.py
+++
@@ -1,9 +0,0 @@
-from src.extension import db
-from src.models.base import BaseModel
-
-class user_input(db.Model,BaseModel):
-    __tabelname__ = "stats"
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable=False)
-    weight_input= db.Column(db.String)
-
