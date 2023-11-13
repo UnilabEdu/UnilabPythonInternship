5a7c47ba2d58cef34130bc3900f125ea9caf6ee9
TheMechanicalBeing <tornike.tsulukidze@gmail.com>
2023-11-14 00:25:14 +0400

    Finished CH05 Homework

--- Chapter05_Flask_Templates/Projects/2023/Tornike_Tsulukidze/app.py
+++ Chapter05_Flask_Templates/Projects/2023/Tornike_Tsulukidze/app.py
@@ -1,11 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
+bookerWinnersList = [
+    {
+        "year": 2022,
+        "author": "Shehan Karunatilaka",
+        "book": "The Seven Moons of Maali Almeida",
+        "publisher": "Sort of Books"
+    },
+    {
+        "year": 2021,
+        "author": "Damon Galgue",
+        "book": "The Promise",
+        "publisher": "Chatto & Windus"
+    },
+    {
+        "year": 2020,
+        "author": "Douglas Stuart",
+        "book": "Shuggie Bain",
+        "publisher": "Picador"
+    },
+    {
+        "year": 2019,
+        "author": "Margaret Atwood",
+        "book": "The Testaments",
+        "publisher": "Chatto & Windus",
+    },
+    {
+        "year": 2019,
+        "author": "Bernardine Evaristo",
+        "book": "Girl, Woman, Other",
+        "publisher": "Hamish Hamilton"
+    },
+    {
+        "year": 2018,
+        "author": "Anna Burns",
+        "book": "Milkman",
+        "publisher": "Faber & Faber"
+    }
+]
+
 @app.route("/")
+@app.route("/home")
 def main():
-    return render_template("main.html")
+    return render_template("main.html", title="Home Page", bookerWinnersList=bookerWinnersList)
 
 
 @app.route("/register")
