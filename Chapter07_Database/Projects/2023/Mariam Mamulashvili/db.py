4c6ca9730605ecf13f5c0a6df936aa7a703a597f
mariam mamulashvili <117090624+mea1234@users.noreply.github.com>
2023-12-06 11:56:24 +0400

    Delete Chapter07_Database/Projects/2023/Mariam Mamulashvili directory

--- Chapter07_Database/Projects/2023/Mariam Mamulashvili/db.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from app import flask_app
-
-
-
-db = SQLAlchemy(flask_app)
-
--- Chapter07_Database/Projects/2023/Mariam Mamulashvili/init_db.py
+++
@@ -1,7 +0,0 @@
-from app import flask_app
-from db import db
-from models import Product
-
-
-with flask_app.app_context():
-    db.create_all()
