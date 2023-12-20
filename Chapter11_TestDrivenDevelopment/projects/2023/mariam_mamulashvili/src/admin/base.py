a24bc6a852d340482e7684f499c8c5ffde8204b3
mariam mamulashvili <mariamiii1999@gmail.com>
2023-12-20 18:14:37 +0400

    add tests

--- Chapter11_TestDrivenDevelopment/projects/2023/mariam_mamulashvili/src/admin/base.py
+++ Chapter11_TestDrivenDevelopment/projects/2023/mariam_mamulashvili/src/admin/base.py
@@ -2,7 +2,6 @@ from flask_admin.contrib.sqla import ModelView
 from flask_admin import AdminIndexView, expose
 from flask_login import current_user, logout_user
 from flask import redirect, url_for
-from flask_admin.menu import MenuLink
 
 
 class SecureModelView(ModelView):
