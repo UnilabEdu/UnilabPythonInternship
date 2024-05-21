a3b87e86b2cd00d71557bdf2192f3299c861b4eb
R-Grigala <r.grigalashvili777@gmail.com>
2024-05-22 01:27:09 +0400

    new flask project

--- Chapter05_Flask_Templates/Projects/2024/roma_grigalashvili/app.py
+++ Chapter05_Flask_Templates/Projects/2024/roma_grigalashvili/app.py
@@ -1,10 +1,53 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+top_users_list = [
+    {
+        "username": "Roma Grigalashvili",
+        "gender": "Male",
+        "email": "roma.grigalashvili@iliauni.edu.ge",
+        "subject": "Math"
+    },
+    {
+        "username": "Saba Dvali",
+        "gender": "Male",
+        "email": "saba.dvali@iliauni.edu.ge",
+        "subject": "History"
+    },
+    {
+        "username": "Saba Dvali",
+        "gender": "Male",
+        "email": "saba.dvali@iliauni.edu.ge",
+        "subject": "History"
+    },
+    {
+        "username": "Saba Dvali",
+        "gender": "Male",
+        "email": "saba.dvali@iliauni.edu.ge",
+        "subject": "History"
+    },
+    {
+        "username": "Roma Grigalashili",
+        "gender": "Male",
+        "email": "roma.grigalashvili@iliauni.edu.ge",
+        "subject": "Geography"
+    },
+]
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/highscores")
+def highscores():
+    return render_template("highscores.html", userlist=top_users_list)
+
+
+@app.route("/registration")
+def game():
+    return render_template("registration.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
