3afcc6e8ea498139dfdf5c021c4f5a03a3822dd2
gogasaldadze <165835640+gogasaldadze@users.noreply.github.com>
2024-05-27 01:46:07 +0400

    Homework  (#331)
    * added my folder in project 2024
    
    * დამატება readme.md ფაილში
    
    * Merge remote-tracking branch 'upstream/master'
    
    * added myfolder in projects with completed task
    
    * transfering to my laptop
    
    * done task for chap7

---
+++ Chapter06_Forms/Projects/2024/goga saldadze/data.py
@@ -0,0 +1,24 @@
+prod_list =[ {
+    "name":"acer laptop" , 
+    "description": "i7 32ram",
+    "img":"https://assets3.razerzone.com/2qIEzE6PovR0jcvcBUI6HVJQVKM=/300x300/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fhaf%2Fh7b%2F9720377704478%2Fblade14-p10-black-500x500.png"
+ },
+{
+    "name":"razer laptop" , 
+    "description": "i9 32ram",
+    "img":"https://assets3.razerzone.com/2qIEzE6PovR0jcvcBUI6HVJQVKM=/300x300/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fhaf%2Fh7b%2F9720377704478%2Fblade14-p10-black-500x500.png"
+},
+{
+    "name":"asus laptop" , 
+    "description": "i5 32ram",
+    "img":"https://assets3.razerzone.com/2qIEzE6PovR0jcvcBUI6HVJQVKM=/300x300/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fhaf%2Fh7b%2F9720377704478%2Fblade14-p10-black-500x500.png"
+},
+{
+    "name":"dell laptop" , 
+    "description": "i3 32ram",
+    "img":"https://assets3.razerzone.com/2qIEzE6PovR0jcvcBUI6HVJQVKM=/300x300/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fhaf%2Fh7b%2F9720377704478%2Fblade14-p10-black-500x500.png"
+} 
+]
+
+users=[]
+print(users)
