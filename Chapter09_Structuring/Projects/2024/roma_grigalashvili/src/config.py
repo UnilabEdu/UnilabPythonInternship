5affeea1dd6375e170b4a529337e78cffd793577
R-Grigala <r.grigalashvili777@gmail.com>
2024-05-28 15:49:32 +0400

    Add Flask app with Structuring

--- Chapter09_Structuring/Projects/2024/roma_grigalashvili/src/config.py
+++ Chapter09_Structuring/Projects/2024/roma_grigalashvili/src/config.py
@@ -5,4 +5,4 @@ class Config(object):
     BASE_DIRECTORY = path.abspath(path.dirname(__file__))
 
     SECRET_KEY = "kljadskl10248120318znx"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + path.join(BASE_DIRECTORY, "database.db")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + path.join(BASE_DIRECTORY, "database.sqlite")
