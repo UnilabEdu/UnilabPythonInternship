265c79c2029e8b0ed52ab68895da0977a285804c
Vano Iniashvili <iniashvilivaniko@gmail.com>
2024-05-31 18:47:33 +0400

    Structured project folder

---
+++ Chapter09_Structuring/Projects/2024/Vano_Iniashvili/src/config.py
@@ -0,0 +1,8 @@
+from os import path
+
+class Config(object):
+    BASE_DIRECTORY = path.abspath(path.dirname(__file__))
+    UPLOAD_PATH = path.join(BASE_DIRECTORY, "uploads")
+
+    SECRET_KEY = "secretkey"
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + path.join(BASE_DIRECTORY, 'database.db')
