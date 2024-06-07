fcb3ba7d302b403a16553ee21fc69df9accc68ea
saba <s.dvali@outlook.com>
2024-06-07 15:01:09 +0400

    fix

--- Chapter09_Structuring/Projects/2024/Saba_dvali/src/views/main/routes.py
+++ Chapter09_Structuring/Projects/2024/Saba_dvali/src/views/main/routes.py
@@ -14,6 +14,7 @@ from src.models import Role
 from src.models import Products
 from src.models import Images
 from src.ext import db
+from Functions import add_product
 
 
 TEMPLATE_FOLDER = path.join(Config.PROJECT_ROOT, "templates", "main")
@@ -76,39 +77,6 @@ main_bp = Blueprint("main",__name__, template_folder=TEMPLATE_FOLDER)
 
 #     return "yes"
 
-def add_product():
-    form = AddProductForm()
-    if form.validate_on_submit():
-        product_name =  form.product_name.data
-        product_model =  form.product_model.data
-        price =  str(form.price.data)
-        info =  form.info.data
-
-        addproduct = Products(product_name=product_name,
-                            product_model=product_model,
-                            price=price,
-                            info=info,
-                            user_id=current_user.id)
-        db.session.add(addproduct)
-        db.session.commit()
-        product = db.session.query(Products).filter(Products.product_name == product_name,
-                                                    Products.product_model == product_model,
-                                                    Products.price == price,
-                                                    Products.info == info,
-                                                    Products.user_id == current_user.id
-                                                    ).first()
-        if product:
-            product_images = request.files.getlist('product_images')
-            product_dir = os.path.join(f"{Config.PROJECT_ROOT}/static/images", str(product.id))
-            if not os.path.exists(product_dir):
-                os.makedirs(product_dir)
-                for file in product_images:
-                    if file.filename != '':
-                        filename = secure_filename(file.filename)
-                        file.save(os.path.join(product_dir, filename))
-                        img = Images(image_name=filename,product_id=product.id)
-                db.session.add(img)
-                db.session.commit()
 
 @main_bp.route("/",methods=["GET", "POST"])
 def home():
