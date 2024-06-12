16afb394eeff3fcff4cbc3df128091ceda72c17b
R-Grigala <r.grigalashvili777@gmail.com>
2024-06-13 00:58:03 +0400

    Change HighScores page get from database

--- Chapter12_Docker/Projects/2024/roma_grigalashvili/src/views/main/routes.py
+++ Chapter12_Docker/Projects/2024/roma_grigalashvili/src/views/main/routes.py
@@ -1,44 +1,39 @@
 from flask import render_template, Blueprint
 from os import path
+from src.models import Score
 
 from src.config import Config
 
 TEMPLATES_FOLDER = path.join(Config.BASE_DIRECTORY, "templates", "main")
 main_blueprint = Blueprint("main", __name__, template_folder=TEMPLATES_FOLDER)
 
-top_users_list = [
-    {
-        "username": "Roma Grigalashvili",
-        "email": "roma.grigalashvili@iliauni.edu.ge",
-        "subject": "Math",
-        "score": 40
+# top_users_list = [
+#     {
+#         "username": "Roma Grigalashvili",
+#         "email": "roma.grigalashvili@iliauni.edu.ge",
+#         "quiz": "Quiz in Geography",
+#         "score": 8
 
-    },
-    {
-        "username": "Saba Dvali",
-        "email": "saba.dvali@iliauni.edu.ge",
-        "subject": "History",
-        "score": 38
-    },
-    {
-        "username": "Tea Godoladze",
-        "email": "tea_godoladze@iliauni.edu.ge",
-        "subject": "Math",
-        "score": 37
-    },
-    {
-        "username": "Roma Grigala",
-        "email": "r.grigalashvili777@gmail.com",
-        "subject": "History",
-        "score": 39
-    },
-    {
-        "username": "Albert Buzaladze",
-        "email": "albert.buzaladze.1@iliauni.edu.ge",
-        "subject": "Geography",
-        "score": 37
-    },
-]
+#     },
+#     {
+#         "username": "Saba Dvali",
+#         "email": "saba.dvali@iliauni.edu.ge",
+#         "quiz": "Quiz in Geography",
+#         "score": 8
+#     },
+#     {
+#         "username": "Tea Godoladze",
+#         "email": "tea_godoladze@iliauni.edu.ge",
+#         "quiz": "Quiz in Programmig",
+#         "score": 8
+#     },
+#     {
+#         "username": "Roma Grigala",
+#         "email": "r.grigalashvili777@gmail.com",
+#         "quiz": "Quiz in Programmig",
+#         "score": 9
+#     },
+# ]
 
 
 @main_blueprint.route("/")
@@ -47,4 +42,6 @@ def index():
 
 @main_blueprint.route("/highscores")
 def highscores():
-    return render_template("highscores.html", top_list=top_users_list)
+    top_scores = Score.query.order_by(Score.score.desc()).limit(5).all()
+    
+    return render_template("highscores.html", top_scores=top_scores)
