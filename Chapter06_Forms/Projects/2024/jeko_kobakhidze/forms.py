6ddbbe73521c074ea758d1afa33517af8c5199ad
Jekmen1 <151436749+Jekmen1@users.noreply.github.com>
2024-12-06 18:48:52 +0400

    add flask project (#361)
    * added my name to the inters list
    
    * Update github.io link
    
    * remove unneccessary symbols
    
    * nothing to change
    
    * add flask project
    
    * add forms and validators to the flask project

---
+++ Chapter06_Forms/Projects/2024/jeko_kobakhidze/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+class SurveyForm(FlaskForm):
+    feedback = TextAreaField('Feedback', validators=[DataRequired(), Length(min=10)])
+    submit = SubmitField('Submit')
